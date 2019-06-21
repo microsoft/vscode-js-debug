07cd6829cecb97800f652ce4bab44cdb7a0eb5b6
Pavel Feldman <pavel.feldman@gmail.com>
2019-06-20 22:26:15 -0700

    fix(format): convert tabs into spaces

--- src/adapter.ts
+++ src/adapter.ts
@@ -10,125 +10,125 @@ import * as launcher from './launcher';
 import Protocol from 'devtools-protocol';
 
 export class Adapter implements DAP.Adapter {
-	private _dap: DAP.Connection;
-	private _browserSession: CDPSession;
-	private _targetManager: TargetManager;
-	private _mainTarget: Target;
+  private _dap: DAP.Connection;
+  private _browserSession: CDPSession;
+  private _targetManager: TargetManager;
+  private _mainTarget: Target;
 
-	constructor(dap: DAP.Connection) {
-		this._dap = dap;
-		dap.setAdapter(this);
-	}
+  constructor(dap: DAP.Connection) {
+    this._dap = dap;
+    dap.setAdapter(this);
+  }
 
-	public async initialize(params: DebugProtocol.InitializeRequestArguments): Promise<DebugProtocol.Capabilities> {
-		console.assert(params.linesStartAt1);
-		console.assert(params.columnsStartAt1);
-		console.assert(params.pathFormat === 'path');
+  public async initialize(params: DebugProtocol.InitializeRequestArguments): Promise<DebugProtocol.Capabilities> {
+    console.assert(params.linesStartAt1);
+    console.assert(params.columnsStartAt1);
+    console.assert(params.pathFormat === 'path');
 
-		const executablePath = findChrome().pop();
-		const connection = await launcher.launch(
-			executablePath, {
-				userDataDir: '.profile',
-				pipe: true,
-			});
-		this._targetManager = new TargetManager(connection);
-		this._targetManager.on(TargetEvents.TargetAttached, (target: Target) => {
-			if (target.threadId())
+    const executablePath = findChrome().pop();
+    const connection = await launcher.launch(
+      executablePath, {
+        userDataDir: '.profile',
+        pipe: true,
+      });
+    this._targetManager = new TargetManager(connection);
+    this._targetManager.on(TargetEvents.TargetAttached, (target: Target) => {
+      if (target.threadId())
         this._dap.didChangeThread('started', target.threadId());
-		});
-		this._targetManager.on(TargetEvents.TargetDetached, (target: Target) => {
-			if (target.threadId())
+    });
+    this._targetManager.on(TargetEvents.TargetDetached, (target: Target) => {
+      if (target.threadId())
         this._dap.didChangeThread('exited', target.threadId());
-		});
+    });
 
-		this._browserSession = connection.browserSession();
-		this._browserSession.on(SessionEvents.Disconnected, () => this._dap.didExit(0));
+    this._browserSession = connection.browserSession();
+    this._browserSession.on(SessionEvents.Disconnected, () => this._dap.didExit(0));
 
-		// params.locale || 'en-US'
-		// params.supportsVariableType
-		// params.supportsVariablePaging
-		// params.supportsRunInTerminalRequest
-		// params.supportsMemoryReferences
+    // params.locale || 'en-US'
+    // params.supportsVariableType
+    // params.supportsVariablePaging
+    // params.supportsRunInTerminalRequest
+    // params.supportsMemoryReferences
 
-		this._dap.didInitialize();
-		return {
-			supportsConfigurationDoneRequest: false,
-			supportsFunctionBreakpoints: false,
-			supportsConditionalBreakpoints: false,
-			supportsHitConditionalBreakpoints: false,
-			supportsEvaluateForHovers: false,
-			exceptionBreakpointFilters: [],
-			supportsStepBack: false,
-			supportsSetVariable: false,
-			supportsRestartFrame: false,
-			supportsGotoTargetsRequest: false,
-			supportsStepInTargetsRequest: false,
-			supportsCompletionsRequest: false,
-			supportsModulesRequest: false,
-			additionalModuleColumns: [],
-			supportedChecksumAlgorithms: [],
-			supportsRestartRequest: false,
-			supportsExceptionOptions: false,
-			supportsValueFormattingOptions: false,
-			supportsExceptionInfoRequest: false,
-			supportTerminateDebuggee: false,
-			supportsDelayedStackTraceLoading: false,
-			supportsLoadedSourcesRequest: false,
-			supportsLogPoints: false,
-			supportsTerminateThreadsRequest: false,
-			supportsSetExpression: false,
-			supportsTerminateRequest: false,
-			//supportsDataBreakpoints: false,
-			//supportsReadMemoryRequest: false,
-			//supportsDisassembleRequest: false,
-		};
-	}
+    this._dap.didInitialize();
+    return {
+      supportsConfigurationDoneRequest: false,
+      supportsFunctionBreakpoints: false,
+      supportsConditionalBreakpoints: false,
+      supportsHitConditionalBreakpoints: false,
+      supportsEvaluateForHovers: false,
+      exceptionBreakpointFilters: [],
+      supportsStepBack: false,
+      supportsSetVariable: false,
+      supportsRestartFrame: false,
+      supportsGotoTargetsRequest: false,
+      supportsStepInTargetsRequest: false,
+      supportsCompletionsRequest: false,
+      supportsModulesRequest: false,
+      additionalModuleColumns: [],
+      supportedChecksumAlgorithms: [],
+      supportsRestartRequest: false,
+      supportsExceptionOptions: false,
+      supportsValueFormattingOptions: false,
+      supportsExceptionInfoRequest: false,
+      supportTerminateDebuggee: false,
+      supportsDelayedStackTraceLoading: false,
+      supportsLoadedSourcesRequest: false,
+      supportsLogPoints: false,
+      supportsTerminateThreadsRequest: false,
+      supportsSetExpression: false,
+      supportsTerminateRequest: false,
+      //supportsDataBreakpoints: false,
+      //supportsReadMemoryRequest: false,
+      //supportsDisassembleRequest: false,
+    };
+  }
 
-	async launch(params: DebugProtocol.LaunchRequestArguments): Promise<void> {
-		// params.noDebug
-		// params.url
-		this._mainTarget = this._targetManager.mainTarget();
-		if (!this._mainTarget)
-		  this._mainTarget = await new Promise(f => this._targetManager.once(TargetEvents.TargetAttached, f));
-		this._targetManager.on(TargetEvents.TargetDetached, (target: Target) => {
+  async launch(params: DebugProtocol.LaunchRequestArguments): Promise<void> {
+    // params.noDebug
+    // params.url
+    this._mainTarget = this._targetManager.mainTarget();
+    if (!this._mainTarget)
+      this._mainTarget = await new Promise(f => this._targetManager.once(TargetEvents.TargetAttached, f));
+    this._targetManager.on(TargetEvents.TargetDetached, (target: Target) => {
       if (target === this._mainTarget) {
-				this._dap.didTerminate();
-			}
-		});
-		await this._mainTarget.session().send('Page.navigate', {url: (params as {url:string}).url});
-	}
+        this._dap.didTerminate();
+      }
+    });
+    await this._mainTarget.session().send('Page.navigate', {url: (params as {url:string}).url});
+  }
 
-	async getThreads(): Promise<DebugProtocol.Thread[]> {
-		return this._targetManager.threadTargets().map(target => {
-			return {
-				id: target.threadId(),
-				name: target.threadName(),
-			}
-		});
-	}
+  async getThreads(): Promise<DebugProtocol.Thread[]> {
+    return this._targetManager.threadTargets().map(target => {
+      return {
+        id: target.threadId(),
+        name: target.threadName(),
+      }
+    });
+  }
 
-	async getStackTrace(params: DebugProtocol.StackTraceArguments): Promise<DAP.StackTraceResult> {
-		return {stackFrames: [], totalFrames: 0};
-	}
+  async getStackTrace(params: DebugProtocol.StackTraceArguments): Promise<DAP.StackTraceResult> {
+    return {stackFrames: [], totalFrames: 0};
+  }
 
-	async getScopes(params: DebugProtocol.ScopesArguments): Promise<DebugProtocol.Scope[]> {
-		return [];
-	}
+  async getScopes(params: DebugProtocol.ScopesArguments): Promise<DebugProtocol.Scope[]> {
+    return [];
+  }
 
-	async getVariables(params: DebugProtocol.VariablesArguments): Promise<DebugProtocol.Variable[]> {
-		return [];
-	}
+  async getVariables(params: DebugProtocol.VariablesArguments): Promise<DebugProtocol.Variable[]> {
+    return [];
+  }
 
-	async continue(params: DebugProtocol.ContinueArguments): Promise<void> {
-	}
+  async continue(params: DebugProtocol.ContinueArguments): Promise<void> {
+  }
 
-	async evaluate(args: DebugProtocol.EvaluateArguments): Promise<DAP.EvaluateResult> {
-		if (!this._mainTarget)
-			return {result: '', variablesReference: 0};
-		const params: Protocol.Runtime.EvaluateRequest = {
+  async evaluate(args: DebugProtocol.EvaluateArguments): Promise<DAP.EvaluateResult> {
+    if (!this._mainTarget)
+      return {result: '', variablesReference: 0};
+    const params: Protocol.Runtime.EvaluateRequest = {
       expression: args.expression
-		};
-		const result = await this._mainTarget.session().send('Runtime.evaluate', params) as Protocol.Runtime.EvaluateResponse;
-		return { result: result.result.description, variablesReference: 0 };
-	}
+    };
+    const result = await this._mainTarget.session().send('Runtime.evaluate', params) as Protocol.Runtime.EvaluateResponse;
+    return { result: result.result.description, variablesReference: 0 };
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -10,54 +10,54 @@ import * as DAP from './dap';
 import { Adapter } from './adapter';
 
 export function activate(context: vscode.ExtensionContext) {
-	const provider = new MockConfigurationProvider();
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('cdp', provider));
+  const provider = new MockConfigurationProvider();
+  context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('cdp', provider));
 
-	const factory = new MockDebugAdapterDescriptorFactory();
-	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('cdp', factory));
-	context.subscriptions.push(factory);
+  const factory = new MockDebugAdapterDescriptorFactory();
+  context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('cdp', factory));
+  context.subscriptions.push(factory);
 }
 
 export function deactivate() {
-	// nothing to do
+  // nothing to do
 }
 
 class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
-	/**
-	 * Massage a debug configuration just before a debug session is being launched,
-	 * e.g. add all missing attributes to the debug configuration.
-	 */
-	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+  /**
+   * Massage a debug configuration just before a debug session is being launched,
+   * e.g. add all missing attributes to the debug configuration.
+   */
+  resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
 
-		// if launch.json is missing or empty
-		if (!config.type && !config.request && !config.name) {
-			config.type = 'cdp';
-			config.name = 'Launch';
-			config.request = 'launch';
-		}
-		return config;
-	}
+    // if launch.json is missing or empty
+    if (!config.type && !config.request && !config.name) {
+      config.type = 'cdp';
+      config.name = 'Launch';
+      config.request = 'launch';
+    }
+    return config;
+  }
 }
 
 class MockDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
-	private server?: Net.Server;
+  private server?: Net.Server;
 
-	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-		if (!this.server) {
-			// start listening on a random port
-			this.server = Net.createServer(socket => {
-				const dap = DAP.createConnection(<NodeJS.ReadableStream>socket, socket);
-				new Adapter(dap);
-			}).listen(0);
-		}
+  createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+    if (!this.server) {
+      // start listening on a random port
+      this.server = Net.createServer(socket => {
+        const dap = DAP.createConnection(<NodeJS.ReadableStream>socket, socket);
+        new Adapter(dap);
+      }).listen(0);
+    }
 
-		// make VS Code connect to debug server
-		return new vscode.DebugAdapterServer(this.server.address().port);
-	}
+    // make VS Code connect to debug server
+    return new vscode.DebugAdapterServer(this.server.address().port);
+  }
 
-	dispose() {
-		if (this.server) {
-			this.server.close();
-		}
-	}
+  dispose() {
+    if (this.server) {
+      this.server.close();
+    }
+  }
 }
