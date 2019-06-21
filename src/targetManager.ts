05559d3d1f6500553e91fead4d89db95d7570f82
Dmitry Gozman <dgozman@gmail.com>
2019-06-21 11:58:31 -0700

    feat: threads

--- src/targetManager.ts
+++ src/targetManager.ts
@@ -4,6 +4,7 @@ import * as debug from 'debug';
 import * as path from 'path';
 import { URL } from 'url';
 import { EventEmitter } from 'events';
+import {Thread} from './thread';
 const debugTarget = debug('target');
 
 export const TargetEvents = {
@@ -22,12 +23,8 @@ export class TargetManager extends EventEmitter {
     this._connection = connection;
     this._browserSession = connection.browserSession();
     this._attachToFirstPage();
-    this._browserSession.on('Target.targetInfoChanged', (event: Protocol.Target.TargetInfoChangedEvent)=> {
-      const target = this._targets.get(event.targetInfo.targetId);
-      if (target) {
-        target._updateFromInfo(event.targetInfo);
-        this._targetInfoChanged(target);
-      }
+    this._browserSession.on('Target.targetInfoChanged', (event: Protocol.Target.TargetInfoChangedEvent) => {
+      this._targetInfoChanged(event.targetInfo);
     });
   }
 
@@ -35,23 +32,18 @@ export class TargetManager extends EventEmitter {
     return Array.from(this._targets.values());
   }
 
-  threadTargets(): Target[] {
+  threads(): Thread[] {
     const mainTarget = this.mainTarget();
     if (!mainTarget)
       return [];
 
-    const result: Target[] = [];
-    mainTarget._visit(t => t.threadId() ? result.push(t) : 0);
+    const result: Thread[] = [];
+    mainTarget._visit(t => t.thread() ? result.push(t.thread()) : 0);
     return result;
   }
 
   mainTarget(): Target {
     return this._targets.values().next().value;
-  }
-
-  target(sequenceNumber: number): Target {
-    // TODO(pfeldman): map it.
-    return this.targets().find(t => t.threadId() === sequenceNumber);
   }
 
   _attachToFirstPage() {
@@ -87,7 +79,11 @@ export class TargetManager extends EventEmitter {
     this._dumpTargets();
   }
 
-  _targetInfoChanged(target: Target) {
+  _targetInfoChanged(targetInfo: Protocol.Target.TargetInfo) {
+    const target = this._targets.get(targetInfo.targetId);
+    if (!target)
+      return;
+    target._updateFromInfo(targetInfo);
     this.emit(TargetEvents.TargetChanged, target);
     this._dumpTargets();
   }
@@ -104,15 +100,13 @@ const jsTypes = new Set(['page', 'iframe', 'worker']);
 export class Target {
   private _children: Map<Protocol.Target.TargetID, Target> = new Map();
   private _targetManager: TargetManager;
-  private _targetInfo: Protocol.Target.TargetInfo;
+  _targetInfo: Protocol.Target.TargetInfo;
   _targetId: Protocol.Target.TargetID;
   private _session: CDPSession;
-  private static _lastThreadId: number = 0;
-  private _threadId = ++Target._lastThreadId;
-  private _threadName: string;
-  private _parentTarget: Target | null;
+  private _parentTarget?: Target;
+  _thread: Thread | undefined;
 
-  constructor(targetManager: TargetManager, targetInfo: Protocol.Target.TargetInfo, session: CDPSession, waitingForDebugger: boolean, parentTarget: Target | null) {
+  constructor(targetManager: TargetManager, targetInfo: Protocol.Target.TargetInfo, session: CDPSession, waitingForDebugger: boolean, parentTarget: Target | undefined) {
     this._targetManager = targetManager;
     this._targetId = targetInfo.targetId;
     this._session = session;
@@ -128,19 +122,16 @@ export class Target {
     });
     this._parentTarget = parentTarget;
 
-    this._threadId = jsTypes.has(targetInfo.type) ? ++Target._lastThreadId : 0;
+    if (jsTypes.has(targetInfo.type))
+      this._thread = new Thread(this);
     this._updateFromInfo(targetInfo);
 
-    debugTarget(`Attached to ${this._threadName}`);
+    debugTarget(`Attached to ${this._targetInfo.title}`);
     this._initialize(waitingForDebugger);
   }
 
-  threadId(): number {
-    return this._threadId;
-  }
-
-  threadName(): string {
-    return this._threadName;
+  thread(): Thread | undefined {
+    return this._thread;
   }
 
   session(): CDPSession {
@@ -148,13 +139,16 @@ export class Target {
   }
 
   async _initialize(waitingForDebugger: boolean) {
-    await this._session.send('Runtime.enable');
+    if (this._thread)
+      await this._thread.initialize();
     if (waitingForDebugger)
       this._session.send('Runtime.runIfWaitingForDebugger');
   }
 
   _updateFromInfo(targetInfo: Protocol.Target.TargetInfo) {
     this._targetInfo = targetInfo;
+    if (!this._thread)
+      return;
 
     let indentation = '';
     for (let parent = this._parentTarget; parent; parent = parent._parentTarget) {
@@ -177,20 +171,22 @@ export class Target {
       if (parsedURL.pathname === '/')
         threadName += parsedURL.host;
       else
-        threadName += parsedURL ? path.basename(parsedURL.pathname) + (parsedURL.hash ? parsedURL.hash : '') : `#${this._threadId}`;
+        threadName += parsedURL ? path.basename(parsedURL.pathname) + (parsedURL.hash ? parsedURL.hash : '') : `#${this._thread.threadId()}`;
     } catch (e) {
       threadName += targetInfo.url;
     }
-    this._threadName = threadName;
-    debugTarget(`Target updated ${this._threadName}`);
+
+    this._thread.setThreadName(threadName);
   }
 
   _dispose() {
     for (const child of this._children.values())
       child._dispose();
+    if (this._thread)
+      this._thread.dispose();
     this._targetManager._detachedFromTarget(this);
     this._session.dispose();
-    debugTarget(`Detached from ${this._threadName}`);
+    debugTarget(`Detached from ${this._targetInfo.title}`);
   }
 
   _visit(visitor: (t: Target) => void) {
