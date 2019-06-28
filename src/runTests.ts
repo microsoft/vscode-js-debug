d011ddf1de795d901fe727353333df14f979c185
Dmitry Gozman <dgozman@gmail.com>
2019-06-28 10:45:02 -0700

    chore: some tests for threads

--- src/runTests.ts
+++ src/runTests.ts
@@ -6,6 +6,7 @@ import * as test from './test/test';
 async function runTests() {
   await test.suite('infra');
   await test.suite('stepping/pause');
+  await test.suite('stepping/threads');
   await test.report();
   process.exit(0);
 }
--- src/test/stepping/pause.ts
+++ src/test/stepping/pause.ts
@@ -7,14 +7,16 @@ async function pausedFromInitialScript(p: test.Params) {
   p.dap.launch({url: 'data:text/html,<script>debugger;</script>'});
   const event = await p.dap.once('stopped');
   p.log(event);
+  p.log(await p.dap.stackTrace({threadId: event.threadId}));
   p.log(await p.dap.continue({threadId: event.threadId}));
 }
 
 async function pausedFromEval(p: test.Params) {
   await p.dap.launch({url: 'data:text/html,blank'});
-  p.cdp.Runtime.evaluate({expression: 'debugger;'});
+  p.cdp.Runtime.evaluate({expression: '\n\ndebugger;\n//# sourceURL=eval.js'});
   const event = await p.dap.once('stopped');
   p.log(event);
+  p.log(await p.dap.stackTrace({threadId: event.threadId}));
   p.log(await p.dap.continue({threadId: event.threadId}));
 }
 
