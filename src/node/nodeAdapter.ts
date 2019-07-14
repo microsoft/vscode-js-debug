bbdd9c67f4623dea40ec4ec0c60f6d9522f18d5d
Pavel Feldman <pavel.feldman@gmail.com>
2019-07-14 15:38:00 -0700

    feat(node): name the threads

--- src/node/nodeAdapter.ts
+++ src/node/nodeAdapter.ts
@@ -23,6 +23,7 @@ export interface LaunchParams extends Dap.LaunchParams {
 
 interface Endpoint {
   pid: string;
+  scriptName: string | undefined;
   inspectorUrl: string;
 }
 
@@ -138,6 +139,12 @@ export class NodeAdapter {
     this._connections.push(connection);
     const cdp = connection.createSession('');
     const thread = this._adapter.threadManager.createThread(cdp, false);
+    let threadName;
+    if (info.scriptName)
+      threadName = `${path.basename(info.scriptName)} [${info.pid}]`;
+    else
+      threadName = `[${info.pid}]`;
+    thread.setThreadDetails(threadName, threadName, '');
     connection.onDisconnected(() => thread.dispose());
     await thread.initialize();
   }
