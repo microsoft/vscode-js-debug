231f5d439f7ad04140526bf21e66eac1efd05b3f
Pavel Feldman <pavel.feldman@gmail.com>
2019-07-18 14:37:22 -0700

    fix(sourceMap): load source maps from file:// and data:

--- src/node/watchdog.ts
+++ src/node/watchdog.ts
@@ -6,20 +6,32 @@ import { WebSocketTransport, PipeTransport } from '../cdp/transport';
 
 const targetInfo = JSON.parse(process.env.NODE_INSPECTOR_TARGET_INFO!);
 
+function debugLog(text: string) {
+}
+
 process.on('exit', () => {
+  debugLog('KILL');
   process.kill(+targetInfo.targetId);
 });
 
 (async() => {
   const target = await WebSocketTransport.create(targetInfo.url);
+  debugLog('CONNECTED TO TARGET');
   let server: PipeTransport;
   let pipe: any;
   await new Promise(f => pipe = net.createConnection(process.env.NODE_INSPECTOR_IPC!, f));
   server = new PipeTransport(pipe, pipe);
   server.send(JSON.stringify({ method: 'Target.targetCreated', params: { targetInfo } }));
+  debugLog('CONNECTED TO SERVER');
 
   target.onmessage = data => server.send(data);
   server.onmessage = data => target.send(data);
-  target.onclose = () => server.close();
-  server.onclose = () => target.close();
+  target.onclose = () => {
+    debugLog('TARGET CLOSED');
+    server.close();
+  }
+  server.onclose = () => {
+    debugLog('SERVER CLOSED');
+    target.close();
+  }
 })();
