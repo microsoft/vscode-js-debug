6f6388a4dbb2e212759d60ef1104c5fef427593b
Pavel Feldman <pavel.feldman@gmail.com>
2019-07-18 17:15:04 -0700

    feat(test): generated api for test harness

---
+++ src/test/variables/helper.ts
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import { TestP } from '../test';
+import Dap from '../../dap/api';
+
+export async function logVariable(variable: Dap.Variable, p: TestP, indent?: string) {
+  indent = indent || '';
+  const namedCount = variable.namedVariables ? `{${variable.namedVariables}}` : '';
+  const indexedCount = variable.namedVariables ? `[${variable.indexedVariables}]` : '';
+  p.log(`${indent}${variable.name}${namedCount}${indexedCount}: ${variable.type} = ${variable.value}`);
+  if (variable.variablesReference) {
+    const result = await p.dap.variables({ variablesReference: variable.variablesReference });
+    for (const variable of result.variables)
+      logVariable(variable, p, indent + '  ');
+  }
+}
+
