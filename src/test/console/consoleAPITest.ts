64a8125934a5c4988d423d1ae9e6a144f02dc496
Pavel Feldman <pavel.feldman@gmail.com>
2019-07-22 14:27:18 -0700

    feat(console): table layout implemented

--- src/test/console/consoleAPITest.ts
+++ src/test/console/consoleAPITest.ts
@@ -25,4 +25,54 @@ export function addTests(testRunner) {
       p.assertLog();
     });
   });
+
+  it('format string', async ({ p }: { p: TestP }) => {
+    await p.launchAndLoad(`<script>
+    var peopleObject = {
+      one: ["John", "Smith"],
+      two: ["Jane", "Doe"],
+      three: ["Emily", "Jones"]
+    };
+    var peopleObject2 = {
+      one: { name: "John", last: "Smith"},
+      two: { name: "Jane", last: "Doe"},
+      three: { name: "Emily", last: "Jones"}
+    };
+    var peopleLongHeader = {
+      one: { "first name": "John", "last name": "Smith"},
+      two: { "first name": "Jane", "last name": "Doe"},
+      three: { "first name": "Emily", "last name": "Jones"}
+    };
+    var peopleArray= [
+      ["John", "Smith"],
+      ["Jane", "Doe"],
+      ["Emily", "Jones"]
+    ];
+    var trimEmptyColumn = [
+      ["John", "Smith", ""],
+      ["Jane", "Doe", ""],
+      ["Emily", "Jones", ""]
+    ];
+    var cellOverflow = {
+      one: ["John", "Smith"],
+      two: ["Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane Jane ",
+       "Doe Doe Doe Doe Doe Doe Doe DoeDoe DoeDoe Doe Doe DoeDoe DoeDoe DoeDoe Doe"],
+      three: ["Emily", "Jones"]
+    };
+    var longTableOverflow = [
+      new Array(1000).fill(0),
+      new Array(1000).fill(1)
+    ];
+    </script>`);
+    await p.logger.evaluateAndLog([
+      `console.table(peopleObject)`,
+      `console.table(peopleObject2)`,
+      `console.table(peopleLongHeader)`,
+      `console.table(peopleArray)`,
+      `console.table(trimEmptyColumn)`,
+      `console.table(cellOverflow)`,
+      `console.table(longTableOverflow)`,
+    ], 0);
+    p.assertLog();
+  });
 }
