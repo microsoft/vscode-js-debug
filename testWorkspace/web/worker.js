a13914b5632fd50da517904119b3b057305670f7
Dmitry Gozman <dgozman@gmail.com>
2019-07-26 18:08:21 -0700

    feature(stack): cross-thread async stacks

--- testWorkspace/web/worker.js
+++ testWorkspace/web/worker.js
@@ -2,5 +2,8 @@ console.error({foo: {bar: 'worker start'}});
 
 self.addEventListener('message', e => {
   console.error(e.data);
-  postMessage({foo: {bar: 'to page'}});
+  if (e.data === 'pause')
+    postMessage('pause');
+  else
+    postMessage({foo: {bar: 'to page'}});
 });
