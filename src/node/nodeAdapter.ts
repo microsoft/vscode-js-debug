9f17dc486c028f663de0fa9ab57f03ed0cce92ff
Pavel Feldman <pavel.feldman@gmail.com>
2019-07-28 15:36:46 -0700

    feat(node): allow passing environment variables when launching

--- src/node/nodeAdapter.ts
+++ src/node/nodeAdapter.ts
@@ -22,6 +22,7 @@ export interface LaunchParams extends Dap.LaunchParams {
   args: string[];
   runtimeExecutable: string;
   cwd: string;
+  env: Object;
 }
 
 let counter = 0;
@@ -66,7 +67,7 @@ export class NodeAdapter {
 
     this._runtime = spawn(executable, this._launchParams!.args || [], {
       cwd: this._launchParams!.cwd || this._debugAdapter.workspaceFolder,
-      env: env(this._pipe!),
+      env: env(this._pipe!, this._launchParams!.env || {}),
       stdio: ['ignore', 'pipe', 'pipe']
     });
     const outputName = [executable, ...(this._launchParams!.args || [])].join(' ');
@@ -199,9 +200,10 @@ export class NodeAdapter {
   }
 }
 
-function env(pipe: string) {
+function env(pipe: string, env: object) {
   const result = {
     ...process.env,
+    ...env,
     NODE_OPTIONS: `${process.env.NODE_OPTIONS|| ''} --require bootloader.js`,
     NODE_PATH: `${process.env.NODE_PATH || ''}${path.delimiter}${path.join(__dirname)}`,
     NODE_INSPECTOR_IPC: pipe
