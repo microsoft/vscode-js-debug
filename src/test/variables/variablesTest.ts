15fc4bc6f1d20f1b006f4fbddaed3e140e31d49f
Dmitry Gozman <dgozman@gmail.com>
2019-07-29 09:54:06 -0700

    test: add tests for output slots and selected context

--- src/test/variables/variablesTest.ts
+++ src/test/variables/variablesTest.ts
@@ -12,7 +12,7 @@ export function addTests(testRunner) {
   describe('basic', () => {
     it('basic object', async ({ p }: { p: TestP }) => {
       await p.launchAndLoad('blank');
-      await p.logger.logEvaluateResult('({a: 1})');
+      await p.logger.evaluateAndLog('({a: 1})');
       p.assertLog();
     });
 
@@ -53,19 +53,19 @@ export function addTests(testRunner) {
   describe('object', () => {
     it('simple array', async ({ p }: { p: TestP }) => {
       await p.launchAndLoad('blank');
-      await p.logger.logEvaluateResult('var a = [1, 2, 3]; a.foo = 1; a', { logInternalInfo: true});
+      await p.logger.evaluateAndLog('var a = [1, 2, 3]; a.foo = 1; a', { logInternalInfo: true});
       p.assertLog();
     });
 
     it('large array', async ({ p }: { p: TestP }) => {
       await p.launchAndLoad('blank');
-      await p.logger.logEvaluateResult('var a = new Array(110); a.fill(1); a', { logInternalInfo: true });
+      await p.logger.evaluateAndLog('var a = new Array(110); a.fill(1); a', { logInternalInfo: true });
       p.assertLog();
     });
 
     it('get set', async ({ p }: { p: TestP }) => {
       await p.launchAndLoad('blank');
-      await p.logger.logEvaluateResult(`
+      await p.logger.evaluateAndLog(`
         const a = {};
         Object.defineProperty(a, 'getter', { get: () => {} });
         Object.defineProperty(a, 'setter', { set: () => {} });
@@ -76,7 +76,7 @@ export function addTests(testRunner) {
 
     it('deep accessor', async ({ p }: { p: TestP }) => {
       await p.launchAndLoad('blank');
-      await p.logger.logEvaluateResult(`
+      await p.logger.evaluateAndLog(`
         class Foo { get getter() {} }
         class Bar extends Foo { }
         new Bar();`);
@@ -90,7 +90,7 @@ export function addTests(testRunner) {
         <meta name='foo' content='bar'></meta>
         <title>Title</title>
       </head>`);
-      await p.logger.logEvaluateResult('document.head.children');
+      await p.logger.evaluateAndLog('document.head.children');
       p.assertLog();
     });
   });
@@ -112,7 +112,7 @@ export function addTests(testRunner) {
   describe('setVariable', () => {
     it('basic', async({p} : {p: TestP}) => {
       await p.launchAndLoad('blank');
-      const v = await p.logger.logEvaluateResult(`window.x = ({foo: 42}); x`);
+      const v = await p.logger.evaluateAndLog(`window.x = ({foo: 42}); x`);
 
       p.log(`\nSetting "foo" to "{bar: 17}"`);
       const response = await p.dap.setVariable({variablesReference: v.variablesReference, name: 'foo', value: '{bar: 17}'});
