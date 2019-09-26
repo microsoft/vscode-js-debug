871bcd636b5aed6bb4c454abd85e7abcf9f00fd7
Dmitry Gozman <dgozman@gmail.com>
2019-09-26 14:36:22 -0700

    refactor: merge AdapterFactory and SessionManager

--- src/adapterFactory.ts
+++ src/adapterFactory.ts
@@ -6,7 +6,6 @@ import * as vscode from 'vscode';
 import { DebugAdapter } from './adapter/debugAdapter';
 import { Binder, BinderDelegate } from './binder';
 import DapConnection from './dap/connection';
-import { SessionManager } from './sessionManager';
 import { BrowserLauncher } from './targets/browser/browserLauncher';
 import { NodeLauncher } from './targets/node/nodeLauncher';
 import { BrowserAttacher } from './targets/browser/browserAttacher';
@@ -17,12 +16,14 @@ import { FileSourcePathResolver } from './common/sourcePathResolver';
 import { TerminalProgramLauncher } from './ui/terminalProgramLauncher';
 
 export class Session implements Disposable {
+  _debugSession: vscode.DebugSession;
   private _server: net.Server;
-  private _debugAdapter?: DebugAdapter;
+  _debugAdapter?: DebugAdapter;
   private _binder?: Binder;
   private _onTargetNameChanged?: Disposable;
 
   constructor(context: vscode.ExtensionContext, debugSession: vscode.DebugSession, target: Target | undefined, binderDelegate: BinderDelegate | undefined, callback: (debugAdapter: DebugAdapter) => void) {
+    this._debugSession = debugSession;
     if (target && checkVersion('1.39.0'))
       this._onTargetNameChanged = target.onNameChanged(() => debugSession.name = target.name());
 
@@ -62,39 +63,109 @@ export class Session implements Disposable {
   }
 }
 
-export class AdapterFactory implements vscode.DebugAdapterDescriptorFactory, Disposable {
+export class AdapterFactory implements vscode.DebugAdapterDescriptorFactory, Disposable, BinderDelegate {
   private _context: vscode.ExtensionContext;
-  private _disposables: Disposable[];
+  private _disposables: Disposable[] = [];
   private _sessions = new Map<string, Session>();
-  private _sessionManager: SessionManager;
+
+  private _pendingTarget = new Map<string, Target>();
+  private _sessionForTarget = new Map<Target, Promise<Session>>();
+  private _sessionForTargetCallbacks = new Map<Target, { fulfill: (session: Session) => void, reject: (error: Error) => void}>();
 
   constructor(context: vscode.ExtensionContext) {
     this._context = context;
     context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('pwa', this));
     context.subscriptions.push(this);
-    this._disposables = [];
-    this._sessionManager = new SessionManager();
 
     vscode.debug.onDidTerminateDebugSession(debugSession => {
       const session = this._sessions.get(debugSession.id);
-      if (!session)
-        return;
       this._sessions.delete(debugSession.id);
-      session.dispose();
+      if (session)
+        session.dispose();
     }, undefined, this._disposables);
   }
 
   createDebugAdapterDescriptor(debugSession: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-    const session = this._sessionManager.createSession(this._context, debugSession, debugAdapter => {});
+    let session: Session;
+
+    if (debugSession.configuration['__pendingTargetId']) {
+      const pendingTargetId = debugSession.configuration['__pendingTargetId'] as string;
+      const target = this._pendingTarget.get(pendingTargetId)!;
+      this._pendingTarget.delete(pendingTargetId);
+
+      session = new Session(this._context, debugSession, target, undefined, debugAdapter => {
+        debugAdapter.dap.on('attach', async () => {
+          const callbacks = this._sessionForTargetCallbacks.get(target);
+          this._sessionForTargetCallbacks.delete(target);
+          if (callbacks)
+            callbacks.fulfill(session);
+          return {};
+        });
+      });
+    } else {
+      session = new Session(this._context, debugSession, undefined, this, () => {});
+    }
+
     this._sessions.set(debugSession.id, session);
     return session.descriptor();
+  }
+
+  async acquireDebugAdapter(target: Target): Promise<DebugAdapter> {
+    const session = await this._createSession(target);
+    return session._debugAdapter!;
+  }
+
+  _createSession(target: Target): Promise<Session> {
+    if (!this._sessionForTarget.has(target)) {
+      this._sessionForTarget.set(target, new Promise<Session>(async (fulfill, reject) => {
+        this._pendingTarget.set(target.id(), target);
+        this._sessionForTargetCallbacks.set(target, {fulfill, reject});
+
+        let parentDebugSession: vscode.DebugSession;
+        const parentTarget = target.parent();
+        if (parentTarget) {
+          const parentSession = await this._createSession(parentTarget);
+          parentDebugSession = parentSession._debugSession;
+        } else {
+          parentDebugSession = this._sessions.get(target.targetOrigin() as string)!._debugSession;
+        }
+
+        const config = {
+          type: 'pwa',
+          name: target.name(),
+          request: 'attach',
+          __pendingTargetId: target.id()
+        };
+
+        if (checkVersion('1.39.0')) {
+          vscode.debug.startDebugging(parentDebugSession.workspaceFolder, config, {
+            parentSession: parentDebugSession,
+            consoleMode: vscode.DebugConsoleMode.MergeWithParent
+          });
+        } else {
+          vscode.debug.startDebugging(parentDebugSession.workspaceFolder, config, parentDebugSession);
+        }
+      }));
+    }
+    return this._sessionForTarget.get(target)!;
+  }
+
+  releaseDebugAdapter(target: Target, debugAdapter: DebugAdapter) {
+    debugAdapter.dap.terminated({});
+    this._sessionForTarget.delete(target);
+    const callbacks = this._sessionForTargetCallbacks.get(target);
+    if (callbacks)
+      callbacks.reject(new Error('Target gone'));
+    this._sessionForTargetCallbacks.delete(target);
   }
 
   dispose() {
     for (const session of this._sessions.values())
       session.dispose();
     this._sessions.clear();
-    this._sessionManager.dispose();
+    this._pendingTarget.clear();
+    this._sessionForTarget.clear();
+    this._sessionForTargetCallbacks.clear();
     for (const disposable of this._disposables)
       disposable.dispose();
     this._disposables = [];
