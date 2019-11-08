2e52650de54cf091efd708b7acb1ceab5f8ccc83
Eric Cornelson <Eric.Cornelson@microsoft.com>
2019-11-08 11:12:02 -0600

    fix: get the package build target working (#73)
    * fix: get the package build target working
    
    These changes get both the parcel bundling and vsix build targets working together.
    
    A few changes to note:
    - Separated the packaging phase to use the dist folder instead of out. This helps keep packaging completely separate from the normal debug/dev builds
    - As a result of the above change, the vsce package command now runs inside of the dist folder, so we need to copy any other needed files there as well (atm that's package.json, package.nls.json, and LICENSE)
    - Changed the gulpfile to use the command-line version of vsce instead of the js lib. I found the js version very hard to debug because gulp tends to swallow console.log statements, so the build would fail with zero diagnostic output. Using the command line api solves that problem and makes builds easier to debug.
    - Moved the tsconfig out of the src dir and into the root. I did this mainly because we are referencing the package.json in some parts of the code now, and if we use require(''), parcel can't resolve it, because we've gone one level deeper (from src to out/src). The solution to this is to use typescript import to import package.json. On compile, ts knows to change the relative path based on the output dir. But in order to let ts actually do this, we have to change the root of the ts project so that package.json is inside of it.
    
    * chore: switch from parcel to webpack
    
    Parcel seems to have issues with the vscode dependency on Mac/Linux?
    
    * fix: always use forward slashes in gulp src/dest
    
    gulp seems to be able to handle backslashes on windows, but is not always consistent in behavior (e.g. in this case, on windows gulp would copy the resources directory fully recursively, whereas on mac/linux it would only copy the files, not preserving the directory structure)
    
    * fix: don't stub __dirname, copy shell scripts

--- gulpfile.js
+++ gulpfile.js
@@ -10,20 +10,28 @@ const path = require('path');
 const replace = require('gulp-replace');
 const sourcemaps = require('gulp-sourcemaps');
 const ts = require('gulp-typescript');
+const rename = require('gulp-rename');
+const merge = require('merge2');
 const tslint = require('gulp-tslint');
 const typescript = require('typescript');
 const vsce = require('vsce');
-const fs = require('fs');
-const cp = require('child_process');
+const webpack = require('webpack');
+const execSync = require('child_process').execSync;
 
 const dirname = 'vscode-node-debug3';
-const extensionId = 'node-debug3';
+const extensionId = 'vscode-pwa';
 const translationProjectName = 'vscode-extensions';
 const translationExtensionName = 'vscode-node-debug';
 
 const sources = ['src/**/*.ts'];
 const tslintFilter = ['**', '!**/*.d.ts'];
 const allPackages = [];
+
+const buildDir = 'out';
+const buildSrcDir = `${buildDir}/src`;
+const distDir = 'dist';
+const distSrcDir = `${distDir}/src`;
+const nodeTargetsDir = `targets/node`;
 
 /**
  * If --drop-in is set, commands and debug types will be set to 'chrome' and
@@ -33,7 +41,7 @@ const allPackages = [];
 const namespace = process.argv.includes('--drop-in') ? '' : 'pwa-';
 
 const replaceNamespace = () => replace(/NAMESPACE\((.*?)\)/g, `${namespace}$1`);
-const tsProject = ts.createProject('./src/tsconfig.json', { typescript });
+const tsProject = ts.createProject('./tsconfig.json', { typescript });
 const tslintOptions = {
   formatter: 'prose',
   rulesDirectory: 'node_modules/tslint-microsoft-contrib',
@@ -55,7 +63,7 @@ gulp.task('compile:ts', () =>
         sourceRoot: '.',
       }),
     )
-    .pipe(gulp.dest('out/out')),
+    .pipe(gulp.dest('out')),
 );
 
 gulp.task(
@@ -63,51 +71,95 @@ gulp.task(
   gulp.parallel(
     () =>
       gulp
-        .src(['*.json', 'resources/**/*'], { base: '.' })
+        .src('*.json')
         .pipe(replaceNamespace())
         .pipe(gulp.dest('out')),
     () =>
       gulp
         .src('src/**/*.sh')
         .pipe(replaceNamespace())
-        .pipe(gulp.dest('out/out')),
+        .pipe(gulp.dest('out/src')),
   ),
 );
 
 gulp.task('compile', gulp.parallel('compile:ts', 'compile:static'));
 
-gulp.task('package:copy-modules', () => {
-  // vsce wants to run `npm ls` to verify modules in the target package. For
-  // this, they need to exist. Copy our production dependencies into the out
-  // directory so this works and they can be bundled. We need to walk the
-  // dependency list recursively to get any hoisted/deduped modules.
+/** Run webpack to bundle the extension output files */
+gulp.task('package:webpack-bundle', async () => {
+  const packages = [
+    { entry: `${buildSrcDir}/extension.js`, library: true },
+    { entry: `${buildSrcDir}/${nodeTargetsDir}/bootloader.js`, library: false },
+    { entry: `${buildSrcDir}/${nodeTargetsDir}/watchdog.js`, library: false },
+  ];
 
-  const prodModules = [];
-  function walk(tree) {
-    for (const key of Object.keys(tree.dependencies || {})) {
-      prodModules.push(key);
-      walk(tree.dependencies[key]);
+  for (const { entry, library } of packages) {
+    const config = {
+      mode: 'production',
+      target: 'node',
+      entry: path.resolve(entry),
+      output: {
+        path: path.resolve(distSrcDir),
+        filename: path.basename(entry),
+        devtoolModuleFilenameTemplate: '../[resource-path]',
+      },
+      node: {
+        __dirname: false,
+        __filename: false,
+      },
+      externals: {
+        vscode: 'commonjs vscode',
+      },
+    };
+
+    if (library) {
+      config.output.libraryTarget = 'commonjs2';
     }
+
+    await new Promise((resolve, reject) =>
+      webpack(config, (err, stats) => {
+        if (err) {
+          reject(err);
+        } else if (stats.hasErrors()) {
+          reject(stats);
+        } else {
+          resolve();
+        }
+      }),
+    );
   }
-
-  walk(JSON.parse(cp.execSync('npm ls --prod --json')));
-
-  return gulp
-    .src(`node_modules/{${prodModules.join(',')}}/**/*`)
-    .pipe(gulp.dest('out/node_modules'));
 });
 
-gulp.task('package:vsix', () =>
-  vsce.createVSIX({
-    ...minimist(process.argv.slice(2)),
-    cwd: 'out',
-    packagePath: path.join(__dirname, 'out', `${extensionId}.vsix`),
-  }),
+/** Copy the extension static files */
+gulp.task('package:copy-extension-files', () =>
+  merge(
+    gulp.src([
+      `${buildDir}/package.json`,
+      `${buildDir}/package.nls.json`,
+      'LICENSE',
+    ]),
+    gulp.src('resources/**/*', { base: '.' }),
+    gulp.src(`src/**/*.sh`).pipe(rename({ dirname: 'src' })),
+  ).pipe(gulp.dest(distDir)),
 );
 
+/** Create a VSIX package using the vsce command line tool */
+gulp.task('package:createVSIX', () =>
+  runCommand(
+    `${path.join('..', 'node_modules', '.bin', 'vsce')} package --yarn -o ${extensionId}.vsix`,
+    { stdio: 'inherit', cwd: distDir },
+  ),
+);
+
+/** Clean, compile, bundle, and create vsix for the extension */
 gulp.task(
   'package',
-  gulp.series('clean', gulp.parallel('compile', 'package:copy-modules'), 'package:vsix'),
+  gulp.series(
+    'clean',
+    'compile',
+    'package:webpack-bundle',
+    'package:copy-extension-files',
+    'package:createVSIX',
+  ),
 );
 
 gulp.task(
@@ -151,7 +203,7 @@ gulp.task(
     gulp
       .src(['out/package.json', 'out/nls.metadata.header.json', 'out/nls.metadata.json'])
       .pipe(nls.createXlfFiles(translationProjectName, translationExtensionName))
-      .pipe(gulp.dest(path.join('..', 'vscode-translations-export'))),
+      .pipe(gulp.dest(`../vscode-translations-export`)),
   ),
 );
 
@@ -171,3 +223,20 @@ gulp.task('lint', () =>
     .pipe(tslint(tslintOptions))
     .pipe(tslint.report()),
 );
+
+/**
+ * Run a command in the terminal using exec, and wrap it in a promise
+ * @param {string} cmd The command line command + args to execute
+ * @param {ExecOptions} options, see here for options: https://nodejs.org/docs/latest-v10.x/api/child_process.html#child_process_child_process_exec_command_options_callback
+ */
+function runCommand(cmd, options) {
+  return new Promise((resolve, reject) => {
+    let execError = undefined;
+    try {
+      execSync(cmd, { stdio: 'inherit', ...options });
+    } catch (err) {
+      reject(err);
+    }
+    resolve();
+  });
+}
