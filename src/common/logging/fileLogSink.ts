bf86e8099f72ed483f7546f765a552155f4a46f2
Connor Peet <connor@peet.io>
2019-12-09 15:22:13 -0800

    revert: fix: file log appending issues, addition session target logs
    This reverts commit 909d8dced55316c175d7acfc0cc0e6cb39c90d74, which was
    causing mysterious process crashes across multiple platforms.

--- src/common/logging/fileLogSink.ts
+++ src/common/logging/fileLogSink.ts
@@ -19,78 +19,20 @@ const replacer = (_key: string, value: unknown): any => {
   return value;
 };
 
-const enum FileStreamState {
-  Inactive,
-  Open,
-  Closing,
-}
-
-type FileStreamData =
-  | { state: FileStreamState.Inactive }
-  | { state: FileStreamState.Open; stream: WriteStream }
-  | { state: FileStreamState.Closing; queue: string[] };
-
-class FileStream {
-  private s: FileStreamData = { state: FileStreamState.Inactive };
-
-  constructor(private readonly path: string) {}
-
-  public close() {
-    if (this.s.state !== FileStreamState.Open) {
-      return;
-    }
-
-    const next: FileStreamData = { state: FileStreamState.Closing, queue: [] };
-    this.s.stream.end(() => {
-      if (!next.queue.length) {
-        this.s = { state: FileStreamState.Inactive };
-        return;
-      }
-
-      const stream = createWriteStream(this.path);
-      this.s = { state: FileStreamState.Open, stream };
-      next.queue.forEach(d => stream.write(d));
-    });
-  }
-
-  public write(data: string) {
-    switch (this.s.state) {
-      case FileStreamState.Inactive:
-        try {
-          mkdirSync(dirname(this.path), { recursive: true });
-        } catch {
-          // already exists
-        }
-
-        this.s = { state: FileStreamState.Open, stream: createWriteStream(this.path) };
-      // fall through
-      case FileStreamState.Open:
-        this.s.stream.write(data);
-        break;
-      case FileStreamState.Closing:
-        this.s.queue.push(data);
-        break;
-      default:
-        throw new Error(`Unknown state ${this.s}`);
-    }
-  }
-}
-
 /**
  * A log sink that writes to a file.
  */
 export class FileLogSink implements ILogSink {
-  private static readonly streams = new Map<string, FileStream>();
-  private stream?: FileStream;
+  private stream?: WriteStream;
 
   constructor(private readonly file: string, private readonly dap: Dap.Api) {
-    let stream = FileLogSink.streams.get(file);
-    if (!stream) {
-      stream = new FileStream(file);
-      FileLogSink.streams.set(file, stream);
+    try {
+      mkdirSync(dirname(file), { recursive: true });
+    } catch {
+      // already exists
     }
 
-    this.stream = stream;
+    this.stream = createWriteStream(file);
   }
 
   /**
@@ -108,7 +50,7 @@ export class FileLogSink implements ILogSink {
    */
   public dispose() {
     if (this.stream) {
-      this.stream.close();
+      this.stream.end();
       this.stream = undefined;
     }
   }
--- src/ui/sessionManager.ts
+++ src/ui/sessionManager.ts
@@ -19,8 +19,6 @@ import { NodeAttacher } from '../targets/node/nodeAttacher';
 import { ExtensionHostLauncher } from '../targets/node/extensionHostLauncher';
 import { ExtensionHostAttacher } from '../targets/node/extensionHostAttacher';
 import { TerminalNodeLauncher } from '../targets/node/terminalNodeLauncher';
-import { logger } from '../common/logging/logger';
-import { LogTag } from '../common/logging';
 
 export class Session implements Disposable {
   public readonly debugSession: vscode.DebugSession;
@@ -32,17 +30,15 @@ export class Session implements Disposable {
   constructor(debugSession: vscode.DebugSession) {
     this.debugSession = debugSession;
     this.connection = new DapConnection();
-    this._server = net
-      .createServer(async socket => {
-        this.connection.init(socket, socket);
-      })
-      .listen(0);
+    this._server = net.createServer(async socket => {
+      this.connection.init(socket, socket);
+    }).listen(0);
   }
 
   listenToTarget(target: Target) {
     if (checkVersion('1.39.0'))
       this._onTargetNameChanged = target.onNameChanged(() => {
-        this.debugSession.name = target.name();
+        this.debugSession.name = target.name()
       });
   }
 
@@ -64,53 +60,45 @@ export class Session implements Disposable {
   }
 
   dispose() {
-    if (this._binder) this._binder.dispose();
-    if (this._onTargetNameChanged) this._onTargetNameChanged.dispose();
+    if (this._binder)
+      this._binder.dispose();
+    if (this._onTargetNameChanged)
+      this._onTargetNameChanged.dispose();
     this._server.close();
   }
 }
 
-export class SessionManager
-  implements vscode.DebugAdapterDescriptorFactory, Disposable, BinderDelegate {
+export class SessionManager implements vscode.DebugAdapterDescriptorFactory, Disposable, BinderDelegate {
   private _disposables: Disposable[] = [];
   private _sessions = new Map<string, Session>();
 
   private _pendingTarget = new Map<string, Target>();
   private _sessionForTarget = new Map<Target, Promise<Session>>();
-  private _sessionForTargetCallbacks = new Map<
-    Target,
-    { fulfill: (session: Session) => void; reject: (error: Error) => void }
-  >();
+  private _sessionForTargetCallbacks = new Map<Target, { fulfill: (session: Session) => void, reject: (error: Error) => void}>();
 
   constructor(private readonly _context: vscode.ExtensionContext) {}
 
   public terminate(debugSession: vscode.DebugSession) {
     const session = this._sessions.get(debugSession.id);
     this._sessions.delete(debugSession.id);
-    if (session) session.dispose();
+    if (session)
+      session.dispose();
   }
 
-  createDebugAdapterDescriptor(
-    debugSession: vscode.DebugSession,
-    executable: vscode.DebugAdapterExecutable | undefined,
-  ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+  createDebugAdapterDescriptor(debugSession: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
     const session = new Session(debugSession);
     if (debugSession.configuration['__pendingTargetId']) {
       const pendingTargetId = debugSession.configuration['__pendingTargetId'] as string;
-      logger.verbose(LogTag.Internal, 'Resolve pending target', {
-        targetId: pendingTargetId,
-      });
-
       const target = this._pendingTarget.get(pendingTargetId)!;
       this._pendingTarget.delete(pendingTargetId);
-      session.listenToTarget(target);
+      session.listenToTarget(target); 
       const callbacks = this._sessionForTargetCallbacks.get(target);
       this._sessionForTargetCallbacks.delete(target);
-      if (callbacks) callbacks.fulfill(session);
+      if (callbacks)
+        callbacks.fulfill(session);
     } else {
       session.createBinder(this._context, this);
     }
-
     this._sessions.set(debugSession.id, session);
     return session.descriptor();
   }
@@ -122,45 +110,35 @@ export class SessionManager
 
   _createSession(target: Target): Promise<Session> {
     if (!this._sessionForTarget.has(target)) {
-      this._sessionForTarget.set(
-        target,
-        new Promise<Session>(async (fulfill, reject) => {
-          logger.verbose(LogTag.Internal, 'Create pending target', {
-            targetId: target.id(),
+      this._sessionForTarget.set(target, new Promise<Session>(async (fulfill, reject) => {
+        this._pendingTarget.set(target.id(), target);
+        this._sessionForTargetCallbacks.set(target, {fulfill, reject});
+
+        let parentDebugSession: vscode.DebugSession;
+        const parentTarget = target.parent();
+        if (parentTarget) {
+          const parentSession = await this._createSession(parentTarget);
+          parentDebugSession = parentSession.debugSession;
+        } else {
+          parentDebugSession = this._sessions.get(target.targetOrigin() as string)!.debugSession;
+        }
+
+        const config = {
+          type: Contributions.ChromeDebugType,
+          name: target.name(),
+          request: 'attach',
+          __pendingTargetId: target.id()
+        };
+
+        if (checkVersion('1.39.0')) {
+          vscode.debug.startDebugging(parentDebugSession.workspaceFolder, config, {
+            parentSession: parentDebugSession,
+            consoleMode: vscode.DebugConsoleMode.MergeWithParent
           });
-          this._pendingTarget.set(target.id(), target);
-          this._sessionForTargetCallbacks.set(target, { fulfill, reject });
-
-          let parentDebugSession: vscode.DebugSession;
-          const parentTarget = target.parent();
-          if (parentTarget) {
-            const parentSession = await this._createSession(parentTarget);
-            parentDebugSession = parentSession.debugSession;
-          } else {
-            parentDebugSession = this._sessions.get(target.targetOrigin() as string)!.debugSession;
-          }
-
-          const config = {
-            type: Contributions.ChromeDebugType,
-            name: target.name(),
-            request: 'attach',
-            __pendingTargetId: target.id(),
-          };
-
-          if (checkVersion('1.39.0')) {
-            vscode.debug.startDebugging(parentDebugSession.workspaceFolder, config, {
-              parentSession: parentDebugSession,
-              consoleMode: vscode.DebugConsoleMode.MergeWithParent,
-            });
-          } else {
-            vscode.debug.startDebugging(
-              parentDebugSession.workspaceFolder,
-              config,
-              parentDebugSession,
-            );
-          }
-        }),
-      );
+        } else {
+          vscode.debug.startDebugging(parentDebugSession.workspaceFolder, config, parentDebugSession);
+        }
+      }));
     }
     return this._sessionForTarget.get(target)!;
   }
@@ -172,17 +150,20 @@ export class SessionManager
   releaseDap(target: Target) {
     this._sessionForTarget.delete(target);
     const callbacks = this._sessionForTargetCallbacks.get(target);
-    if (callbacks) callbacks.reject(new Error('Target gone'));
+    if (callbacks)
+      callbacks.reject(new Error('Target gone'));
     this._sessionForTargetCallbacks.delete(target);
   }
 
   dispose() {
-    for (const session of this._sessions.values()) session.dispose();
+    for (const session of this._sessions.values())
+      session.dispose();
     this._sessions.clear();
     this._pendingTarget.clear();
     this._sessionForTarget.clear();
     this._sessionForTargetCallbacks.clear();
-    for (const disposable of this._disposables) disposable.dispose();
+    for (const disposable of this._disposables)
+      disposable.dispose();
     this._disposables = [];
   }
 }
