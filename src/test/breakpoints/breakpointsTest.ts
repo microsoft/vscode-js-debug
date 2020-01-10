467b6d1009023d791ad5a057e8dc516b502e86bf
Rob Lourens <roblourens@gmail.com>
2020-01-10 11:53:14 -0800

    just breakpoint tests

--- src/test/breakpoints/breakpointsTest.ts
+++ src/test/breakpoints/breakpointsTest.ts
@@ -11,7 +11,7 @@ import { readfile } from '../../common/fsUtils';
 import { forceForwardSlashes } from '../../common/pathUtils';
 import del = require('del');
 
-describe('breakpoints', () => {
+describe.only('breakpoints', () => {
   async function waitForPause(p: ITestHandle, cb?: () => Promise<void>) {
     const { threadId } = p.log(await p.dap.once('stopped'));
     await p.logger.logStackTrace(threadId);
