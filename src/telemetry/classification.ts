5de1b34c420044ad247b91e3114274aa832b3888
Connor Peet <connor@peet.io>
2020-02-12 08:02:46 -0800

    fix: convert launch data to k/v pairs for classification

--- src/telemetry/classification.ts
+++ src/telemetry/classification.ts
@@ -134,14 +134,33 @@ interface ILaunchClassification {
   os: { classification: 'SystemMetaData'; purpose: 'FeatureInsight' };
 }
 
+interface IKeyValuePair<T> {
+  key: string;
+  value: T;
+}
+
 export interface ILaunchMetrics {
   type: string;
   os: string;
   nodeVersion: string;
   adapterVersion: string;
   request: string;
-  parameters: object;
+  parameters: IKeyValuePair<unknown>[];
 }
+
+const toDeepPairs = (obj: unknown, prefix = ''): IKeyValuePair<unknown>[] => {
+  if (typeof obj !== 'object' || !obj) {
+    return [{ key: prefix, value: obj }];
+  }
+
+  let output: IKeyValuePair<unknown>[] = [];
+  const cast = obj as { [key: string]: unknown };
+  for (const key of Object.keys(cast)) {
+    output = output.concat(toDeepPairs(cast[key], prefix ? `${prefix}.${key}` : key));
+  }
+
+  return output;
+};
 
 /******************************************************************************
  * Implementations
@@ -218,11 +237,12 @@ export const createLoggers = (sendEvent: (event: Dap.OutputEventParams) => void)
     >('js-debug/nodeRuntime', { ...globalMetrics, ...metrics });
   };
 
-  const launch = (metrics: ILaunchMetrics) =>
+  const launch = (metrics: Omit<ILaunchMetrics, 'parameters'> & { parameters: object }) => {
     publicLog2<IGlobalMetrics & ILaunchMetrics, ILaunchClassification & IGlobalClassification>(
       'js-debug/launch',
-      { ...globalMetrics, ...metrics },
+      { ...globalMetrics, ...metrics, parameters: toDeepPairs(metrics.parameters) },
     );
+  };
 
   return {
     breakpointStats,
