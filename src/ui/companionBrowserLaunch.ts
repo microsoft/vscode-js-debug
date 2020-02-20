d287e6ce7b010db9c407df4427d9ed207576d4b5
Connor Peet <connor@peet.io>
2020-02-20 14:52:15 -0800

    fix: use tunnels API to manage forwarded ports

--- src/ui/companionBrowserLaunch.ts
+++ src/ui/companionBrowserLaunch.ts
@@ -5,14 +5,25 @@
 import * as vscode from 'vscode';
 import Dap from '../dap/api';
 
-const launchCompanionBrowser = async (args: Dap.LaunchBrowserInCompanionEventParams) => {
+const sessionTunnels = new Map<string, vscode.Tunnel>();
+
+const launchCompanionBrowser = async (
+  session: vscode.DebugSession,
+  args: Dap.LaunchBrowserInCompanionEventParams,
+) => {
   try {
-    const uri = await vscode.env.asExternalUri(
-      vscode.Uri.parse(`http://127.0.0.1:${args.serverPort}`),
-    );
+    const tunnel = await vscode.workspace.openTunnel({
+      remoteAddress: { port: args.serverPort, host: 'localhost' },
+      localAddressPort: args.serverPort,
+      label: 'Browser Debug Tunnel',
+    });
+
+    sessionTunnels.set(session.id, tunnel);
 
     await vscode.commands.executeCommand('js-debug-companion.launchAndAttach', {
-      proxyUri: uri.authority,
+      proxyUri: tunnel
+        ? `${tunnel.remoteAddress.host}:${tunnel.remoteAddress.port}`
+        : `127.0.01:${args.serverPort}`,
       ...args,
     });
   } catch (e) {
@@ -20,17 +31,28 @@ const launchCompanionBrowser = async (args: Dap.LaunchBrowserInCompanionEventPar
   }
 };
 
-const killCompanionBrowser = ({ launchId }: Dap.KillCompanionBrowserEventParams) =>
+const killCompanionBrowser = (
+  session: vscode.DebugSession,
+  { launchId }: Dap.KillCompanionBrowserEventParams,
+) => {
   vscode.commands.executeCommand('js-debug-companion.kill', { launchId });
+  disposeSessionTunnel(session);
+};
+
+const disposeSessionTunnel = (session: vscode.DebugSession) => {
+  sessionTunnels.get(session.id)?.dispose();
+  sessionTunnels.delete(session.id);
+};
 
 export function registerCompanionBrowserLaunch(context: vscode.ExtensionContext) {
   context.subscriptions.push(
+    vscode.debug.onDidTerminateDebugSession(disposeSessionTunnel),
     vscode.debug.onDidReceiveDebugSessionCustomEvent(async event => {
       switch (event.event) {
         case 'launchBrowserInCompanion':
-          return launchCompanionBrowser(event.body);
+          return launchCompanionBrowser(event.session, event.body);
         case 'killCompanionBrowser':
-          return killCompanionBrowser(event.body);
+          return killCompanionBrowser(event.session, event.body);
         default:
         // ignored
       }
