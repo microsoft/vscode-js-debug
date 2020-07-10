98ef3a38ef11f21ee0e5f59d2ae55c71270a2977
Connor Peet <connor@peet.io>
2020-07-10 10:02:24 -0700

    fix: be more permissive around resolveSourceMapLocations

--- src/configuration.ts
+++ src/configuration.ts
@@ -712,7 +712,7 @@ const nodeBaseDefaults: INodeBaseConfiguration = {
   sourceMaps: true,
   localRoot: null,
   remoteRoot: null,
-  resolveSourceMapLocations: ['${workspaceFolder}/**', '!**/node_modules/**'],
+  resolveSourceMapLocations: ['**', '!**/node_modules/**'],
   autoAttachChildProcesses: true,
   runtimeSourcemapPausePatterns: [],
 };
