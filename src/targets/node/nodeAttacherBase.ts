6031ab4457ef2058f0c76b824da4d33f33a22d49
Connor Peet <connor@peet.io>
2020-07-11 12:10:20 -0700

    fix: use legacy attachment mode for dealing with --inspect-brk (#585)

--- src/targets/node/nodeAttacherBase.ts
+++ src/targets/node/nodeAttacherBase.ts
@@ -2,66 +2,14 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import { AnyNodeConfiguration } from '../../configuration';
-import { NodeLauncherBase, IRunData, IProcessTelemetry } from './nodeLauncherBase';
-import Cdp from '../../cdp/api';
-import { LogTag } from '../../common/logging';
-import { delay } from '../../common/promiseUtil';
 import { injectable } from 'inversify';
+import { AnyNodeConfiguration } from '../../configuration';
+import { NodeLauncherBase } from './nodeLauncherBase';
 
 /**
  * Base class that implements common matters for attachment.
  */
 @injectable()
-export abstract class NodeAttacherBase<T extends AnyNodeConfiguration> extends NodeLauncherBase<T> {
-  /**
-   * Reads telemetry from the process.
-   */
-  protected async gatherTelemetry(
-    cdp: Cdp.Api,
-    run: IRunData<T>,
-  ): Promise<IProcessTelemetry | void> {
-    const telemetry = await cdp.Runtime.evaluate({
-      contextId: 1,
-      returnByValue: true,
-      expression: `({ processId: process.pid, nodeVersion: process.version, architecture: process.arch })`,
-    });
-
-    if (!this.program) {
-      return; // shut down
-    }
-
-    if (telemetry?.exceptionDetails) {
-      if (isProcessNotDefined(telemetry.exceptionDetails)) {
-        this.logger.info(LogTag.RuntimeTarget, 'Process not yet defined, will retry');
-        await delay(10);
-        return this.gatherTelemetry(cdp, run);
-      }
-
-      this.logger.error(
-        LogTag.RuntimeTarget,
-        'Error getting telemetry',
-        telemetry.exceptionDetails,
-      );
-      return;
-    }
-
-    if (!telemetry || !telemetry.result.value) {
-      this.logger.error(LogTag.RuntimeTarget, 'Undefined result getting telemetry');
-      return;
-    }
-
-    const result = telemetry.result.value as IProcessTelemetry;
-
-    run.context.telemetryReporter.report('nodeRuntime', {
-      version: result.nodeVersion,
-      arch: result.architecture,
-    });
-    this.program.gotTelemetery(result);
-
-    return result;
-  }
-}
-
-const isProcessNotDefined = (exception: Cdp.Runtime.ExceptionDetails) =>
-  exception.exception && String(exception.exception.description).includes('process is not defined');
+export abstract class NodeAttacherBase<T extends AnyNodeConfiguration> extends NodeLauncherBase<
+  T
+> {}
--- src/targets/node/nodeLauncherBase.ts
+++ src/targets/node/nodeLauncherBase.ts
@@ -14,9 +14,10 @@ import { CancellationTokenSource } from '../../common/cancellation';
 import { ObservableMap } from '../../common/datastructure/observableMap';
 import { EnvironmentVars } from '../../common/environmentVars';
 import { EventEmitter } from '../../common/events';
-import { ILogger } from '../../common/logging';
+import { ILogger, LogTag } from '../../common/logging';
 import { once } from '../../common/objUtils';
 import { findInPath, forceForwardSlashes } from '../../common/pathUtils';
+import { delay } from '../../common/promiseUtil';
 import { AnyLaunchConfiguration, AnyNodeConfiguration } from '../../configuration';
 import { cannotLoadEnvironmentVars } from '../../dap/errors';
 import { ProtocolError } from '../../dap/protocolError';
@@ -455,7 +456,58 @@ export abstract class NodeLauncherBase<T extends AnyNodeConfiguration> implement
       dispose: () => fs.unlinkSync(nearPath),
     };
   }
+
+  /**
+   * Reads telemetry from the process.
+   */
+  protected async gatherTelemetryFromCdp(
+    cdp: Cdp.Api,
+    run: IRunData<T>,
+  ): Promise<IProcessTelemetry | void> {
+    const telemetry = await cdp.Runtime.evaluate({
+      contextId: 1,
+      returnByValue: true,
+      expression: `({ processId: process.pid, nodeVersion: process.version, architecture: process.arch })`,
+    });
+
+    if (!this.program) {
+      return; // shut down
+    }
+
+    if (telemetry?.exceptionDetails) {
+      if (isProcessNotDefined(telemetry.exceptionDetails)) {
+        this.logger.info(LogTag.RuntimeTarget, 'Process not yet defined, will retry');
+        await delay(10);
+        return this.gatherTelemetryFromCdp(cdp, run);
+      }
+
+      this.logger.error(
+        LogTag.RuntimeTarget,
+        'Error getting telemetry',
+        telemetry.exceptionDetails,
+      );
+      return;
+    }
+
+    if (!telemetry || !telemetry.result.value) {
+      this.logger.error(LogTag.RuntimeTarget, 'Undefined result getting telemetry');
+      return;
+    }
+
+    const result = telemetry.result.value as IProcessTelemetry;
+
+    run.context.telemetryReporter.report('nodeRuntime', {
+      version: result.nodeVersion,
+      arch: result.architecture,
+    });
+    this.program.gotTelemetery(result);
+
+    return result;
+  }
 }
+
+const isProcessNotDefined = (exception: Cdp.Runtime.ExceptionDetails) =>
+  exception.exception && String(exception.exception.description).includes('process is not defined');
 
 function readEnvFile(file: string): { [key: string]: string } {
   if (!fs.existsSync(file)) {
