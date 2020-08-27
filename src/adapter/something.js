5d2fb4251fbd652891dbfeb643d1992c856de497
D <digeff@microsoft.com>
2020-08-27 13:11:32 -0700

    Delete something.js

--- src/adapter/something.js
+++
@@ -1,9 +0,0 @@
-function _generatedCode(defaultValue) {
-  try {
-    return (function (def) {
-      return global.customDebuggerDescription(this, def)
-    })(defaultValue);
-  } catch (e) {
-    return e.stack || e.message || String(e);
-  }
-}
