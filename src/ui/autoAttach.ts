3a78050a82ba0df8e1a7dae0933d32de5bdaf44f
Connor Peet <connor@peet.io>
2020-09-21 11:04:58 -0700

    feat: apply terminal debug options to auto attach

--- src/ui/autoAttach.ts
+++ src/ui/autoAttach.ts
@@ -4,7 +4,8 @@
 import { promises as fs } from 'fs';
 import * as vscode from 'vscode';
 import * as nls from 'vscode-nls';
-import { Commands, registerCommand } from '../common/contributionUtils';
+import { Commands, Configuration, readConfig, registerCommand } from '../common/contributionUtils';
+import { LocalFsUtils } from '../common/fsUtils';
 import { ProxyLogger } from '../common/logging/proxyLogger';
 import { DelegateLauncherFactory } from '../targets/delegate/delegateLauncherFactory';
 import {
@@ -13,7 +14,6 @@ import {
 } from '../targets/node/autoAttachLauncher';
 import { NodeBinaryProvider } from '../targets/node/nodeBinaryProvider';
 import { launchVirtualTerminalParent } from './debugTerminalUI';
-import { LocalFsUtils } from '../common/fsUtils';
 
 const localize = nls.loadMessageBundle();
 
@@ -39,7 +39,12 @@ export function registerAutoAttach(
         fs,
         new LocalFsUtils(fs),
       );
-      await launchVirtualTerminalParent(delegate, inst);
+
+      await launchVirtualTerminalParent(
+        delegate,
+        inst,
+        readConfig(vscode.workspace, Configuration.TerminalDebugConfig),
+      );
 
       inst.onTargetListChanged(() => {
         if (inst.targetList().length === 0 && !disposeTimeout) {
