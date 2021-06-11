e15afff9c6ff99bd83ca39de12533cbf6f1c6058
Ryan Inselmann <ryan@inselmann.org>
2021-06-11 02:43:31 -0500

    fix: error when debugging an html file

--- src/common/urlUtils.ts
+++ src/common/urlUtils.ts
@@ -325,6 +325,9 @@ export function urlToRegex(
   [escapeReStart, escapeReEnd]: [number, number] = [0, aPath.length],
 ) {
   const patterns: string[] = [];
+  const rePrefix = aPath.slice(0, escapeReStart);
+  const reSuffix = aPath.slice(escapeReEnd);
+  const unescapedPath = aPath.slice(escapeReStart, escapeReEnd);
 
   // aPath will often (always?) be provided as a file URI, or URL. Decode it
   // --we'll reencode it as we go--and also create a match for its absolute
@@ -336,7 +339,7 @@ export function urlToRegex(
   //  - For case insensitive systems, we generate a regex like [fF][oO][oO]/(?:💩|%F0%9F%92%A9).[jJ][sS]
   //  - If we didn't de-encode it, the percent would be case-insensitized as
   //    well and we would not include the original character in the regex
-  for (const str of [decodeURI(aPath), fileUrlToAbsolutePath(aPath)]) {
+  for (const str of [decodeURI(unescapedPath), fileUrlToAbsolutePath(unescapedPath)]) {
     if (!str) {
       continue;
     }
@@ -347,7 +350,7 @@ export function urlToRegex(
     let re = '';
     for (let i = 0; i < str.length; i++) {
       const char = str[i];
-      const escapeRegex = i >= escapeReStart && i < escapeReEnd;
+      const escapeRegex = true;
 
       if (isCaseSensitive) {
         urlToRegexChar(char, chars, escapeRegex);
@@ -364,7 +367,7 @@ export function urlToRegex(
     // fancy regex above), replace `file:///c:/` or simple `c:/` patterns with
     // an insensitive drive letter.
     patterns.push(
-      re.replace(
+      `${rePrefix}${re}${reSuffix}`.replace(
         /^(file:\\\/\\\/\\\/)?([a-z]):/i,
         (_, file = '', letter) => `${file}[${letter.toUpperCase()}${letter.toLowerCase()}]:`,
       ),
