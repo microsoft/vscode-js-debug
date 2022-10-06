413b4cc8427c46249f925c3beb2078db15d9c29e
Connor Peet <connor@peet.io>
2022-10-06 11:33:24 -0700

    chore: update to new ai key

--- src/build/generate-contributions.ts
+++ src/build/generate-contributions.ts
@@ -47,7 +47,7 @@ import {
 } from '../configuration';
 import strings from './strings';
 
-const appInsightsKey = 'AIF-d9b70cd4-b9f9-4d70-929b-a071c400b217';
+const appInsightsKey = '0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255';
 
 type OmittedKeysFromAttributes =
   | keyof IMandatedConfiguration
