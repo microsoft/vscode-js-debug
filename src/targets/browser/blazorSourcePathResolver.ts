f42cfa22ae795fb382d1fc6f0915c7c078c28a5b
Thays Grazia <thaystg@gmail.com>
2022-12-16 14:26:25 -0300

    trying to avoid duplicated regex

--- src/targets/browser/blazorSourcePathResolver.ts
+++ src/targets/browser/blazorSourcePathResolver.ts
@@ -55,8 +55,8 @@ export class BlazorSourcePathResolver extends BrowserSourcePathResolver {
       // Blazor files have a file:/// url. Override the default absolutePathToUrlRegexp which returns an http based regexp
       const fileUrl = utils.absolutePathToFileUrl(absolutePath);
       const fileRegexp = utils.urlToRegex(fileUrl);
-      if (this.options.baseUrl)
-        return `${fileRegexp}|${super.absolutePathToUrlRegexp(absolutePath)}`;
+      const fileRegexpSuper = super.absolutePathToUrlRegexp(absolutePath);
+      if (!fileRegexp.includes(fileRegexpSuper)) return `${fileRegexp}|${fileRegexpSuper}`;
       return fileRegexp;
     }
 
