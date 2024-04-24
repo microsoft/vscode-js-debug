eada6e165d0baceecf55f3f81b37599dbd9d2ec6
Tim Neutkens <tim@timneutkens.nl>
2024-04-24 13:52:53 +0200

    Add Turbopack sourcemap alias

--- src/configuration.ts
+++ src/configuration.ts
@@ -977,6 +977,7 @@ export function defaultSourceMapPathOverrides(webRoot: string): { [key: string]:
     'webpack://?:*/*': `${webRoot}/*`,
     'webpack:///([a-z]):/(.+)': '$1:/$2',
     'meteor://💻app/*': `${webRoot}/*`,
+    "turbopack://[project]/*": "${workspaceFolder}/*"
   };
 }
 
