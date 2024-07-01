b2a955adba0a9708b95ac55c13385c4460518bc3
Connor Peet <connor@peet.io>
2024-07-01 16:49:17 -0700

    fix: support default browser detection on remotes (#2030)
    Requires https://github.com/microsoft/vscode-js-debug-companion/pull/35 to query the browser from the remote. Also updates `default-browser` which includes the Windows support I PR'd a while ago, so we can delete some code.
    
    Fixes #2012

--- src/common/defaultBrowserProvider.ts
+++ src/common/defaultBrowserProvider.ts
@@ -2,40 +2,16 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import defaultBrowserModule from 'default-browser';
-import { injectable, inject } from 'inversify';
-import { Execa } from '../ioc-extras';
-import type execaType from 'execa';
-import { ChildProcessError } from './processUtils';
+import { inject, injectable, optional } from 'inversify';
+import type * as vscodeType from 'vscode';
+import { ExtensionLocation, VSCodeApi } from '../ioc-extras';
 import { once } from './objUtils';
 
 export const enum DefaultBrowser {
-  Chrome,
-  Safari,
-  Firefox,
-  Edge,
-  OldEdge,
-  IE,
+  Edge = 'edge',
+  Chrome = 'chrome',
+  Other = 'other',
 }
-
-const substrings = new Map([
-  ['chrome', DefaultBrowser.Chrome],
-  ['safari', DefaultBrowser.Safari],
-  ['firefox', DefaultBrowser.Firefox],
-  ['msedge', DefaultBrowser.Edge],
-  ['microsoft edge', DefaultBrowser.Edge],
-  ['appxq0fevzme2pys62n3e0fbqa7peapykr8v', DefaultBrowser.OldEdge],
-  ['ie.http', DefaultBrowser.IE],
-]);
-
-const getMatchingBrowserInString = (str: string) => {
-  str = str.toLowerCase();
-  for (const [needle, browser] of substrings.entries()) {
-    if (str.includes(needle)) {
-      return browser;
-    }
-  }
-};
 
 /**
  * Class that looks up the default browser on the current platform.
@@ -53,8 +29,8 @@ export const IDefaultBrowserProvider = Symbol('IDefaultBrowserProvider');
 @injectable()
 export class DefaultBrowserProvider implements IDefaultBrowserProvider {
   constructor(
-    @inject(Execa) private readonly execa: typeof execaType,
-    private readonly platform = process.platform,
+    @inject(ExtensionLocation) private readonly location: ExtensionLocation,
+    @optional() @inject(VSCodeApi) private readonly vscode?: typeof vscodeType,
   ) {}
 
   /**
@@ -63,35 +39,17 @@ export class DefaultBrowserProvider implements IDefaultBrowserProvider {
    * default browser.
    * @inheritdoc
    */
-  public lookup = once(() => {
-    if (this.platform === 'win32') {
-      return this.lookupWindows();
+  public lookup = once(async () => {
+    let name: string;
+    if (this.location === 'remote' && this.vscode) {
+      name = await this.vscode.commands.executeCommand('js-debug-companion.defaultBrowser');
     } else {
-      return this.lookupUnix();
+      name = (await import('default-browser').then(d => d.default())).name;
     }
+
+    const match = [DefaultBrowser.Chrome, DefaultBrowser.Edge].find(browser =>
+      name.toLowerCase().includes(browser),
+    );
+    return match ?? DefaultBrowser.Other;
   });
-
-  private async lookupUnix() {
-    const { name } = await defaultBrowserModule();
-    return getMatchingBrowserInString(name);
-  }
-
-  private async lookupWindows() {
-    const result = await this.execa('reg', [
-      'QUERY',
-      ' HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\http\\UserChoice',
-      '/v',
-      'ProgId',
-    ]);
-    if (result.failed) {
-      throw ChildProcessError.fromExeca(result);
-    }
-
-    const match = /ProgId\s*REG_SZ\s*(\S+)/.exec(result.stdout);
-    if (!match) {
-      return undefined;
-    }
-
-    return getMatchingBrowserInString(match[1]);
-  }
 }
