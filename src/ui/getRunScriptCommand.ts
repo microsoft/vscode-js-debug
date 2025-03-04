cd2cc1871a83dc86a43ddffb4bded5765a9fe15a
xymopen_Official <8053733+xymopen@users.noreply.github.com>
2025-03-05 02:07:29 +0800

    Support usenode as npm script runner (#2178)
    * feat: Use `npm.scriptRunner` for resolve script runner
    
    * feat: Support Node.js as script runner

--- src/ui/getRunScriptCommand.ts
+++ src/ui/getRunScriptCommand.ts
@@ -7,16 +7,22 @@ import { commands, WorkspaceFolder } from 'vscode';
 /**
  * Gets the package manager the user configured in the folder.
  */
-export const getPackageManager = async (folder: WorkspaceFolder | undefined) => {
+export const getScriptRunner = async (folder: WorkspaceFolder | undefined) => {
   try {
-    return await commands.executeCommand('npm.packageManager', folder?.uri);
+    return await commands.executeCommand('npm.scriptRunner', folder?.uri);
   } catch {
-    return 'npm';
+    try {
+      return await commands.executeCommand('npm.packageManager', folder?.uri);
+    } catch {
+      return 'npm';
+    }
   }
 };
 
 /**
  * Gets a command to run a script
  */
-export const getRunScriptCommand = async (name: string, folder?: WorkspaceFolder) =>
-  `${await getPackageManager(folder)} run ${name}`;
+export const getRunScriptCommand = async (name: string, folder?: WorkspaceFolder) => {
+  const scriptRunner = await getScriptRunner(folder);
+  return `${scriptRunner} ${scriptRunner === 'node' ? '--run' : 'run'} ${name}`;
+};
